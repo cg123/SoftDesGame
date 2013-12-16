1254c336babfc75455f5ca204c3ad25429b5e278
Charles Goddard <Charles.Goddard@students.olin.edu>
2013-12-15 19:01:23 -0500

    Add python importer to editor scripts

---
+++ Assets/Editor/PythonImporter.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class CustomResxImporter : AssetPostprocessor
+{
+    public static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string asset in importedAssets)
+        {
+            if (asset.EndsWith(".py"))
+            {
+                string filePath = asset.Substring(0, asset.Length - Path.GetFileName(asset).Length) + "../";
+                string newFileName = filePath + Path.GetFileNameWithoutExtension(asset) + ".py.txt";
+
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+
+                StreamReader reader = new StreamReader(asset);
+                string fileData = reader.ReadToEnd();
+                reader.Close();
+
+                FileStream resourceFile = new FileStream(newFileName, FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(resourceFile);
+                writer.Write(fileData);
+                writer.Close();
+                resourceFile.Close();
+
+                AssetDatabase.Refresh(ImportAssetOptions.Default);
+            }
+        }
+    }
+}
